08cc75c4394eb1412873376eb46cd50e616fa230
munene-m <munenenjue18@gmail.com>
2023-05-13 11:28:54 +0300

    Use Mongo.create instead of new keyword

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const path = require('path')
 const dotenv = require("dotenv")
 const session = require('express-session')
-const { MongoStore } = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const { connectDB } = require("./config/db")
 const bodyParser = require("body-parser")
 const authRoute = require('./routes/authRoute')
@@ -28,23 +28,14 @@ const sessionStore = new MongoStore({
   collection: 'user_sessions',
   ttl: 60 * 60 * 24 // session will expire after 1 day (in seconds)
 });
+
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  store: sessionStore
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_CONNECTION_URL})
 }));
-
-
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: false,
-//   store: new MongoStore({
-//     url: process.env.MONGO_CONNECTION_URL,
-//     collection: 'user_sessions',
-//   }),
-// }));
   
 app.use("/auth", authRoute)
 app.use("/predictions", adminRoute)
