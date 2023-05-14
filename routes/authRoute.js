bb47712a12e8799de2b11e50b9e3d0ccaedb3ea9
munene-m <munenenjue18@gmail.com>
2023-05-14 19:09:54 +0300

    added protect middleware to credentials route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const { loginUser, registerUser, getCredentials, loginWithGoogle, googleAuthCallback, updateUser } = require("../controllers/authController");
-
+const { protect } = require("../middleware/authMiddleware")
 const router = express.Router();
-router.route("/credentials").get(getCredentials)
+router.route("/credentials").get(protect,getCredentials)
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/update/:id").put(updateUser)
