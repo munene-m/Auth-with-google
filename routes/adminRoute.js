bd6d46aa8441470a01ad2c0bd059b380e1c620f4
munene-m <munenenjue18@gmail.com>
2023-05-29 22:25:31 +0300

    multer config to handle image uploads to create and update routes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const { createPrediction, updatePrediction, getPrediction, getPredictions, deletePrediction } = require('../controllers/adminController')
+const multer = require("multer")
+const upload = multer({ dest: 'uploads/'})
+const { createPrediction, updatePrediction, getPrediction, getPredictions, getPredictionInCategory, deletePrediction } = require('../controllers/adminController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route("/").get(getPredictions)
 router.route("/:id").get(getPrediction)
-router.route("/create").post(protect,createPrediction)
-router.route("/update/:id").put(protect, updatePrediction)
+router.route("/create").post(
+    protect, upload.fields([
+      { name: 'leagueIcon' },
+      { name: 'teamAIcon' },
+      { name: 'teamBIcon' },
+    ]), createPrediction
+  );
+  
+router.route("/update/:id").put(protect, upload.fields([
+    { name: 'leagueIcon' },
+    { name: 'teamAIcon' },
+    { name: 'teamBIcon' },
+  ]), updatePrediction)
 router.route("/delete/:id").delete(protect, deletePrediction)
+router.route("/prediction/:value").get(protect, getPredictionInCategory)
 
 module.exports = router
