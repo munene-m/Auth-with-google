4aabe25f7d2ca949e5f52406f0974b60066683db
munene-m <munenenjue18@gmail.com>
2023-09-21 23:38:35 +0300

    added update and delete functions

--- controllers/timeController.js
+++ controllers/timeController.js
@@ -15,6 +15,29 @@ const postTime = async (req, res) => {
   }
 };
 
+const updateTime = async(req,res)=>{
+  const time = await Time.findById(req.params.id)
+  if(!time){
+    return res.status(404).json({error:"Not found"})
+  }
+  const updatedTime = await Time.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  res.status(200).json(updatedTime)
+}
+
+const deleteTime = async(req,res)=>{
+  try {
+    const time = await Time.findById(req.params.id)
+    if(!time){
+      return res.status(404).json({error:"Not found"})
+    }
+    await Time.findByIdAndDelete(req.params.id)
+    res.status(200).json({message: "Time post deleted"})
+  } catch (error) {
+    res.status(400).json("An error occured", error);
+  }
+}
+
 const getTime = async (req, res) => {
     try {
       const time = await Time.find();
@@ -24,4 +47,4 @@ const getTime = async (req, res) => {
     }
   };
 
-module.exports = {postTime, getTime}
+module.exports = {postTime, getTime, updateTime, deleteTime}
--- routes/timeRoute.js
+++ routes/timeRoute.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { postTime, getTime } = require("../controllers/timeController")
+const { postTime, getTime, updateTime, deleteTime } = require("../controllers/timeController")
 const { protect } = require("../middleware/authMiddleware")
 router.route("/post").post(protect,postTime)
 router.route("/").get(getTime)
+router.route("/update/:id").put(protect,updateTime)
+router.route("/delete/:id").delete(protect,deleteTime)
+
 
 module.exports = router
