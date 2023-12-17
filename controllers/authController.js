ef6a765b8ba33019c729bd815fced428481b9fba
munene-m <munenenjue18@gmail.com>
2023-12-17 23:11:20 +0300

    chore: move sendVerificationEmail function to its own file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
+const { sendVerificationEmail } = require("../helpers/authHelper.js");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 require("../passport.js");
@@ -134,43 +135,6 @@ const updateUser = async (req, res) => {
   }
 };
 
-const sendVerificationEmail = async (userId, userEmail) => {
-  try {
-    const user = await User.findById(userId);
-    const verificationToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    user.verificationToken = verificationToken;
-    await user.save();
-
-    const mailOptions = {
-      from: process.env.EMAIL_FROM,
-      to: userEmail,
-      subject: "SportyPredict Account Verification",
-      html: `
-        <p>Click the button below to verify your SportyPredict account email.</p>
-        <a href="${process.env.CLIENT_URL}/verify-user/${verificationToken}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px; margin-top: 10px;">Verify Email</a>
-        <p>This link expires in 1 hour.</p>
-      `,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        return res
-          .status(500)
-          .json({ error: "Failed to send account verification email" });
-      }
-
-      console.log("Email sent: " + info.response);
-      res.status(200).json({ message: "Account verification email sent" });
-    });
-  } catch (err) {
-    console.log(err);
-    throw new Error("Failed to send account verification email.");
-  }
-};
-
 const verifyUser = async (req, res) => {
   const { token } = req.params;
 
@@ -228,6 +192,7 @@ const requestPasswordReset = async (req, res) => {
       <p>We received a request to change your password. You can click the button below to proceed with creating a new password.</p>
       <a href="${process.env.CLIENT_URL}/reset-password/${resetToken}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px; margin-top: 10px;">Reset Password</a>
       <p style="margin-top: 10px">If you ignore this email, your password will not be changed. This link expires in 1 hour.</p>
+      
     `,
   };
   transporter.sendMail(mailOptions, (error, info) => {
@@ -319,7 +284,7 @@ const getUsers = async (req, res) => {
   }
 };
 const getUser = async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
   if (!user) {
     res.status(400).json("User not found");
   } else {
