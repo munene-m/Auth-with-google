19520574b7fb38a9e7ff19959d7db3e0105a814a
munene-m <munenenjue18@gmail.com>
2023-12-18 11:26:35 +0300

    chore: use promise to handle errors that may occur when sending email to user

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer");
-const User = require("nodemailer");
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
@@ -12,40 +14,40 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendVerificationEmail = async (userId, userEmail) => {
-  try {
-    const user = await User.findById(userId);
-    const verificationToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    user.verificationToken = verificationToken;
-    await user.save();
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findById(userId);
+      const verificationToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      user.verificationToken = verificationToken;
+      await user.save();
 
-    const mailOptions = {
-      from: process.env.EMAIL_FROM,
-      to: userEmail,
-      subject: "SportyPredict Account Verification",
-      html: `
+      const mailOptions = {
+        from: process.env.EMAIL_FROM,
+        to: userEmail,
+        subject: "SportyPredict Account Verification",
+        html: `
           <p>Click the button below to verify your SportyPredict account email.</p>
           <a href="${process.env.CLIENT_URL}/verify-user/${verificationToken}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px; margin-top: 10px;">Verify Email</a>
           <p>This link expires in 1 hour.</p>
         `,
-    };
+      };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        return res
-          .status(500)
-          .json({ error: "Failed to send account verification email" });
-      }
-
-      console.log("Email sent: " + info.response);
-      res.status(200).json({ message: "Account verification email sent" });
-    });
-  } catch (err) {
-    console.log(err);
-    throw new Error("Failed to send account verification email.");
-  }
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          reject("Failed to send account verification email");
+        } else {
+          console.log("Email sent: " + info.response);
+          resolve("Account verification email sent");
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      reject("Failed to send account verification email");
+    }
+  });
 };
 
-module.exports = sendVerificationEmail;
+module.exports = { sendVerificationEmail };
