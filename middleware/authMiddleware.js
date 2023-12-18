5e1a785ba637d10e252d58dd61476e4b89230be1
munene-m <munenenjue18@gmail.com>
2023-12-18 11:21:48 +0300

    fix: return response in json when token is not passed to prevent server crashing

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,23 +1,26 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+
 const protect = async (req, res, next) => {
   let token;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded.id).select("-password");
 
-    next();
+      next();
+    } catch (error) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
   } else {
-    return res.status(401).json({ error: "Unauthorized attempt" });
-  }
-  if (!token) {
-    return res.status(401).json({ error: "No authorization without token" });
+    return res.status(401).json({ error: "No authorization token provided" });
   }
 };
 
