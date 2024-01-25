9eaeda8c8bdd8528c44e5db9d78577a657d1aab0
munene-m <munenenjue18@gmail.com>
2024-01-25 10:14:12 +0300

    chore: get game by date and team names

--- routes/sportRoute.js
+++ routes/sportRoute.js
@@ -1,23 +1,42 @@
-const express = require('express')
-const router = express.Router()
-const multer = require("multer")
-const upload = multer({ dest: 'uploads/'})
-const { createPrediction, updatePrediction, getPrediction, getPredictionFromSport, getPredictions, deletePrediction } = require('../controllers/sportController')
-const { protect } = require('../middleware/authMiddleware')
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const {
+  createPrediction,
+  updatePrediction,
+  getPrediction,
+  getPredictionFromSport,
+  getPredictions,
+  deletePrediction,
+} = require("../controllers/sportController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route("/create/:sport").post(protect, upload.fields([
-    { name: 'leagueIcon' },
-    { name: 'teamAIcon' },
-    { name: 'teamBIcon' },
-  ]), createPrediction);
-router.route("/update/:id").put(protect, upload.fields([
-    { name: 'leagueIcon' },
-    { name: 'teamAIcon' },
-    { name: 'teamBIcon' },
-  ]), updatePrediction)
-router.route("/prediction/:id").get(getPrediction)
-router.route("/:date").get(getPredictions)
-router.route("/sport/:value/:date").get(getPredictionFromSport)
-router.route("/delete/:id").delete(protect, deletePrediction)
+router
+  .route("/create/:sport")
+  .post(
+    protect,
+    upload.fields([
+      { name: "leagueIcon" },
+      { name: "teamAIcon" },
+      { name: "teamBIcon" },
+    ]),
+    createPrediction
+  );
+router
+  .route("/update/:id")
+  .put(
+    protect,
+    upload.fields([
+      { name: "leagueIcon" },
+      { name: "teamAIcon" },
+      { name: "teamBIcon" },
+    ]),
+    updatePrediction
+  );
+router.route("/single/:date/:teamA/:teamB").get(getPrediction);
+router.route("/:date").get(getPredictions);
+router.route("/sport/:value/:date").get(getPredictionFromSport);
+router.route("/delete/:id").delete(protect, deletePrediction);
 
-module.exports = router
+module.exports = router;
