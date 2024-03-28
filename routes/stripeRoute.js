a1f5995425ed25fd7a388a051159402fae742dbb
munene-m <munenenjue18@gmail.com>
2024-03-28 14:58:23 +0300

    remove middleware from stripe endpoint

--- routes/stripeRoute.js
+++ routes/stripeRoute.js
@@ -1,8 +1,7 @@
 const { createCheckout } = require("../controllers/stripeController.js");
-const { protect } = require("../middleware/authMiddleware");
 const express = require("express");
 const router = express.Router();
 
-router.route("/create-checkout-session").post(protect, createCheckout);
+router.route("/create-checkout-session").post(createCheckout);
 
 module.exports = router;
